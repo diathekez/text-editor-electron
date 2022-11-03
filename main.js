617a98609a026a22d7c7f03868e06309bcc44630
diathekez <96537263+diathekez@users.noreply.github.com>
2022-11-02 19:21:29 -0500

    Saving and creating txt files functions added.

--- main.js
+++ main.js
@@ -8,43 +8,93 @@ const fs = require("fs");
 require("electron-reloader")(module);
 
 let mainWindow;
+let openedFilePath;
 
 const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-        width: 900,
-        height: 700,
-        // Hides default title bar style
-        titleBarStyle: "hiddenInset",
-        // Loads renderer.js and all paths into one
-        webPreferences: {
-            preload: path.join(app.getAppPath(), "renderer.js"),
-        },
-    });
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 700,
+    // Hides default title bar style
+    titleBarStyle: "hiddenInset",
+    // Loads renderer.js and all paths into one
+    webPreferences: {
+      preload: path.join(app.getAppPath(), "renderer.js"),
+    },
+  });
 
-    // Loads browser window with the dev tools already open. This is optional.
-    mainWindow.webContents.openDevTools();
-    mainWindow.loadFile("index.html");
+  // Loads browser window with the dev tools already open. This is optional.
+  mainWindow.webContents.openDevTools();
+  mainWindow.loadFile("index.html");
+
+  //   Adds custom menu to txt editor app
+  const menuTemplate = [
+    {
+      label: "File",
+      submenu: [],
+    },
+  ];
 };
 
 // Controls the lifecycle of the app and returns promise that is fulfilled when Electron is started
 app.whenReady().then(createWindow);
+
+// Shows users any errors
+const handleError = () => {
+  new Notification({
+    title: "Error",
+    body: "Something went wrong!",
+  }).show();
+};
+
+// Opens dialog box that only lets app open .txt files
+ipcMain.on("open-document-triggered", () => {
+  dialog
+    .showOpenDialog({
+      properties: ["openFile"],
+      filters: [{ name: "text files", extensions: ["txt"] }],
+    })
+    .then(({ filePaths }) => {
+      const filePath = filePaths[0];
+
+      // Loads content of file
+      fs.readFile(filePath, "utf8", (error, content) => {
+        if (error) {
+          handleError();
+        } else {
+          openedFilePath = filePath;
+          mainWindow.webContents.send("document-opened", { filePath, content });
+        }
+      });
+    });
+});
+
 // Listens to event from the renderer and calls back a function to be triggered
-ipcMain.on("create-document-triggered", () =>{
-    // Shows user a dialog box to save their text file
-    dialog.showSaveDialog(mainWindow, {
-        // Restricts dialog only to txt files
-        filters: [{ name: "text files", extensions: ["txt"]}],
+ipcMain.on("create-document-triggered", () => {
+  // Shows user a dialog box to save their text file
+  dialog
+    .showSaveDialog(mainWindow, {
+      // Restricts dialog only to txt files
+      filters: [{ name: "text files", extensions: ["txt"] }],
     })
     .then(({ filePath }) => {
-        console.log("file path", filePath);
-        // Saves text file
-        fs.writeFile(filePath, "", (error) => {
-            if (error) {
-                console.log("error")
-            } else {
-                // Sends the event back to renderer
-                mainWindow.webContents.send("document-created", filePath);
-            }
-        });
+      // Saves text file
+      fs.writeFile(filePath, "", (error) => {
+        if (error) {
+          handleError();
+        } else {
+          openedFilePath = filePath;
+          // Sends the event back to renderer
+          mainWindow.webContents.send("document-created", filePath);
+        }
+      });
     });
+});
+
+// Listens for file content updated event
+ipcMain.on("file-content-updated", (_, textareaContent) => {
+  fs.writeFile(openedFilePath, textareaContent, (error) => {
+    if (error) {
+      handleError();
+    }
+  });
 });
--- renderer.js
+++ renderer.js
@@ -1,20 +1,47 @@
 const { ipcRenderer } = require("electron");
+const path = require("path");
 
 window.addEventListener("DOMContentLoaded", () => {
-    // Stores all DOM selectors in one place for easier use
-    const el = {
-        // Selector for element that contains document name
-        documentName: document.getElementById("documentName"),
-        createDocumentBtn: document.getElementById("createDocumentBtn"),
-    };
+  // Stores all DOM selectors in one place for easier use
+  const el = {
+    // Selector for element that contains document name
+    documentName: document.getElementById("documentName"),
+    createDocumentBtn: document.getElementById("createDocumentBtn"),
+    openDocumentBtn: document.getElementById("openDocumentBtn"),
+    fileTextarea: document.getElementById("fileTextarea"),
+  };
 
-    el.createDocumentBtn.addEventListener("click", () => {
-        // Lets the main process know that a click event has happened
-        ipcRenderer.send("create-document-triggered")
-    });
+  // Wraps all code below
+  const handleDocumentChange = (filePath, content = "") => {
+    el.documentName.innerHTML = path.parse(filePath).base;
+    el.fileTextarea.removeAttribute("disabled");
+    el.fileTextarea.value = content;
+    el.fileTextarea.focus();
+  };
 
-    // Callback function: Payload is the filePath
-    ipcRenderer.on("document-created", (_, filePath) => {
-        el.documentName.innerHTML = filePath;
-    });
+  el.createDocumentBtn.addEventListener("click", () => {
+    // Lets the main process know that a click event has happened
+    ipcRenderer.send("create-document-triggered");
+  });
+
+  el.openDocumentBtn.addEventListener("click", () => {
+    ipcRenderer.send("open-document-triggered");
+  });
+
+  // Updates content of file when user is writing in textarea
+  el.fileTextarea.addEventListener("input", (e) => {
+    ipcRenderer.send("file-content-updated", e.target.value);
+  });
+
+  // Listens to load the contents of txt file (document open)
+  ipcRenderer.on("document-opened", (_, { filePath, content }) => {
+    handleDocumentChange(filePath, content);
+  });
+
+  // Callback function: Payload is the filePath.
+  // Disables textarea until a file is opened or created
+  // Focuses textarea when doc is created
+  ipcRenderer.on("document-created", (_, filePath) => {
+    handleDocumentChange(filePath);
+  });
 });
